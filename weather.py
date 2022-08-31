32155fb79b69a1a4c61af9e9f8b6c6a5af56abc5
chenyig001 <56058920+chenyig001@users.noreply.github.com>
2022-08-31 09:36:25 +0800

    Update weather.py
    添加南海区天气

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ import json
 
 app_id = 'wx266a7570f0f5f03f'
 app_secret = '1d181e1501f1b74cc0dcebef81414128'
-template_id = '7WcvYmT44jwWUbAR0sXKSUzXIXfzrveZKREvO2KsdLA'
+template_id = 'gvUhSsFN2Mh498RglcnZeAGWl84GuJsqk1T3z7yr3M8'
 
 access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}'.format(app_id,app_secret)
 res = requests.get(access_token_url).json()
@@ -19,25 +19,29 @@ def weather():
     appcode = 'b4c8a39e21e5415caf458b5f3217d4c6'
     # querys = 'city=%E5%BB%89%E6%B1%9F'
     city = '佛山禅城区'
+    city2 = '佛山南海区'
 
     url = host + path + '?' + 'city={}'.format(city)
+    url2 = host + path + '?' + 'city={}'.format(city2)
 
     header = {"Authorization": "APPCODE b4c8a39e21e5415caf458b5f3217d4c6"}
     content = requests.get(url, headers=header)  # 返回状态码
+    content2 = requests.get(url2, headers=header)  # 返回状态码
     print(content)
     try:
         data = json.loads(content.text)  # 把json字符串转换为字典
+        data2 = json.loads(content2.text)  # 把json字符串转换为字典
     except ValueError:
         print("try again")
     else:
-        result = data.get("result")
-        city = result.get("city")
+        result, result2 = data.get("result"), data2.get("result")
+        city, city2 = result.get("city"), result2.get("city")
         date = result.get("date")
         week = result.get("week")
-        weather = result.get("weather")  # 天气
-        temp = result.get("temp")        # 气温
-        temphigh = result.get("temphigh")
-        templow = result.get("templow")
+        weather, weather2 = result.get("weather"), result2.get("weather")  # 天气
+        temp, temp2 = result.get("temp"), result2.get("temp")        # 气温
+        temphigh, temphigh2 = result.get("temphigh"), result2.get("temphigh")
+        templow, templow2 = result.get("templow"), result2.get("templow")
 
         now = datetime.strftime(datetime.today(), "%Y-%m-%d")
         data = {
@@ -47,6 +51,11 @@ def weather():
             'temp': {'value': temp + '°C'},
             'min_teamperature': {'value': templow + '°C'},
             'max_teamperature': {'value': temphigh + '°C' + '\n'},
+
+            'city2': {'value': city2},
+            'weather2': {'value': weather2, 'color': '#f51616'},
+            'temp2': {'value': temp2 + '°C'},
+            'teamperature': {'value': templow + '~' + temphigh + '°C' + '\n'},
             'msg': {'value': 'BlackPink girl 哈哈哈哈哈。。。', 'color': '#e06666'},
         }
 
